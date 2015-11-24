7b366b21fa7303fe907c62ea2a739aed5c45b855
Bikram Chatterjee <razorpeak@gmail.com>
2015-11-24 11:54:01 +0100

    support starting from any relative path

--- start.sh
+++ start.sh
@@ -13,4 +13,12 @@ if [ "$#" -ne 1 ]; then
     listen_port=""
 fi
 
-$exename -sname $1_smpp -pa deps/*/ebin -pa ebin $listen_port -boot start_sasl -s lager -s smpp_simulator_app
+MY_PATH="`dirname \"$0\"`"              # relative
+MY_PATH="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized
+if [ -z "$MY_PATH" ] ; then
+  # error; for some reason, the path is not accessible
+  # to the script (e.g. permissions re-evaled after suid)
+  exit 1  # fail
+fi
+
+$exename -sname $1_smpp -pa $MY_PATH/deps/*/ebin -pa $MY_PATH/ebin $listen_port -boot start_sasl -s lager -s smpp_simulator_app
